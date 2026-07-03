5d8c341b5594cec12818afd67ae6597fd7842846
rouias11editz-sudo <rouias11editz@gmail.com>
2026-07-03 18:28:58 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -7,23 +7,23 @@ const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
-    try {
-        console.log('⏳ Registering slash commands...');
+  try {
+    console.log('⏳ Registering slash commands...');
 
-        await rest.put(
-            Routes.applicationCommands(process.env.CLIENT_ID),
-            { body: commands }
-        );
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
 
-        console.log('✅ Slash commands registered successfully.');
-    } catch (error) {
-        console.error(error);
-    }
+    console.log('✅ Slash commands registered successfully.');
+  } catch (error) {
+    console.error(error);
+  }
 })();
