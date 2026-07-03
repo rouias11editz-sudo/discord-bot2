637ac8ca3d1d1a045143331a54458317307c907d
rouias11editz-sudo <rouias11editz@gmail.com>
2026-07-03 18:15:14 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,1 +1,29 @@
+const { REST, Routes } = require('discord.js');
+require('dotenv').config();
 
+const fs = require('fs');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log('⏳ Registering slash commands...');
+
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            { body: commands }
+        );
+
+        console.log('✅ Slash commands registered successfully.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
