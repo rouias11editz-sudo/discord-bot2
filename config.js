779069b0145fb145284fd272b1504195a4fdb4ab
rouias11editz-sudo <rouias11editz@gmail.com>
2026-07-05 21:49:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,22 @@
+module.exports = {
+    confessionChannel: "1523425911469506764",
 
+    modAlertChannel: "1510278929666146317",
+
+    lowMediumPing: [
+        "1510273298850910348",
+        "1510273221172396032"
+    ],
+
+    highPing: [
+        "1510271113651818717",
+        "1510273045691109417"
+    ],
+
+    embedColors: {
+        LOW: 8ee89a,       // Green
+        MEDIUM: d2e362,    // Yellow
+        HIGH: e3a262,      // Orange
+        CRITICAL: e36262   // Red
+    }
+};
