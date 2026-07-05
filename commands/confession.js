31f87f3f0c3944f6a49c7d628bef48d9234bb4fc
rouias11editz-sudo <rouias11editz@gmail.com>
2026-07-05 21:54:26 +0100

    Update confession.js

--- commands/confession.js
+++ commands/confession.js
@@ -1,1 +1,36 @@
+const {
+    SlashCommandBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    ModalBuilder,
+    TextInputBuilder,
+    TextInputStyle
+} = require("discord.js");
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("confession")
+        .setDescription("Send an anonymous confession"),
+
+    async execute(interaction) {
+
+        const modal = new ModalBuilder()
+            .setCustomId("confession_modal")
+            .setTitle("Anonymous Confession");
+
+        const confession = new TextInputBuilder()
+            .setCustomId("confession_text")
+            .setLabel("Your confession")
+            .setPlaceholder("Type your confession here...")
+            .setStyle(TextInputStyle.Paragraph)
+            .setRequired(true)
+            .setMaxLength(2000);
+
+        modal.addComponents(
+            new ActionRowBuilder().addComponents(confession)
+        );
+
+        await interaction.showModal(modal);
+    }
+};
