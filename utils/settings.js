ff3f1e9e943a910825f35e281fd02ba2ee332563
rouias11editz-sudo <rouias11editz@gmail.com>
2026-07-10 17:42:15 +0100

    Update settings.js

--- utils/settings.js
+++ utils/settings.js
@@ -1,1 +1,54 @@
+const fs = require("fs");
+const path = require("path");
 
+const FILE = path.join(__dirname, "..", "data", "guilds.json");
+
+function ensureDatabase() {
+    if (!fs.existsSync(FILE)) {
+        fs.writeFileSync(FILE, JSON.stringify({}, null, 4));
+    }
+}
+
+function load() {
+    ensureDatabase();
+    return JSON.parse(fs.readFileSync(FILE, "utf8"));
+}
+
+function save(data) {
+    fs.writeFileSync(FILE, JSON.stringify(data, null, 4));
+}
+
+function getGuild(guildId) {
+    const data = load();
+
+    if (!data[guildId]) {
+        data[guildId] = {
+            confessionChannel: null,
+            modAlertChannel: null,
+            modRoles: [],
+            blistChannel: null
+        };
+
+        save(data);
+    }
+
+    return data[guildId];
+}
+
+function setGuild(guildId, settings) {
+    const data = load();
+
+    data[guildId] = {
+        ...getGuild(guildId),
+        ...settings
+    };
+
+    save(data);
+
+    return data[guildId];
+}
+
+module.exports = {
+    getGuild,
+    setGuild
+};
