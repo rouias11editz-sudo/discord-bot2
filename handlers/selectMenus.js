6a926a53638d175c0b3386d2de0151b3460f6672
rouias11editz-sudo <rouias11editz@gmail.com>
2026-07-10 17:47:29 +0100

    Update selectMenus.js

--- handlers/selectMenus.js
+++ handlers/selectMenus.js
@@ -1,1 +1,51 @@
+const { get, create } = require("../utils/wizardSessions");
 
+module.exports = async (interaction) => {
+
+    const id = interaction.guild.id;
+
+    let session = get(id);
+
+    if (!session)
+        session = create(id);
+
+    // ===========================
+    // Confession Channel
+    // ===========================
+
+    if (interaction.customId === "wizard_confession_channel") {
+
+        session.confessionChannel =
+            interaction.values[0];
+
+        return interaction.deferUpdate();
+
+    }
+
+    // ===========================
+    // Mod Alert Channel
+    // ===========================
+
+    if (interaction.customId === "wizard_modalert_channel") {
+
+        session.modAlertChannel =
+            interaction.values[0];
+
+        return interaction.deferUpdate();
+
+    }
+
+    // ===========================
+    // Moderator Roles
+    // ===========================
+
+    if (interaction.customId === "wizard_mod_roles") {
+
+        session.modRoles =
+            interaction.values;
+
+        return interaction.deferUpdate();
+
+    }
+
+};
